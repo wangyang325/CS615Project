1998498dd87052521155c341cdd825cabbb044a9
wangyang325 <wangyang325@hotmail.com>
2020-03-11 00:23:06 +0000

    Del package

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 module.exports = function ( app ) {
     require('./login')(app);
-    require('./home')(app);
     require('./logout')(app);
     require('./register')(app);
-    require('./cart')(app);
     require('./test')(app);
     require('./editBook')(app);
     require('./listBook')(app);
